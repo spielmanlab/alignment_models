12f471890713604a09fd83f57efa0e507a7133cf
Molly Miraglia <mlmiraglia99@gmail.com>
2019-05-24 15:16:41 -0400

    added to hw7

---
+++ practice/hw7.py
@@ -0,0 +1,26 @@
+#problem 1
+myanimals = ["deer", "wolf", "fox", "dog", "cat"]
+print(myanimals)
+
+#1
+myanimals.sort()
+print (myanimals)
+
+#2 
+x = len(myanimals)
+print(x)
+
+#3
+myanimals.remove("cat")
+print (myanimals)
+
+#4 
+myanimals.append("ostrich")
+
+#5
+
+newlist = myanimals * 3
+print (newlist)
+
+
+
