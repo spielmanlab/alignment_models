944b4a042b3da10f826f58c53d32a470e9c9dd53
Molly Miraglia <mlmiraglia99@gmail.com>
2019-05-25 11:44:18 -0400

    1st modification of subscript1

--- project/sub_script1.py
+++ project/sub_script1.py
@@ -1,3 +1,21 @@
-#python script for 
-insert re
+#python script for iq_sub
+
+import re
+
+
+iqf = open ("iqtree_sub.txt", "r")
+print (iqf)
+
+#can't trust line numbers, can trust format and patterns 
+
+s1 =   "1  Dayhoff       34598.308    63  69322.616    69333.353    69618.917"
+s1.strip()
+
+#start using regular expressions
+#notes for molly when she comes back to this: regular expression operators
+
+
+
+
+
 
