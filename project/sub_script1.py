2b1963847d6e948bcc04976a2635f635dcc05422
Molly Miraglia <mlmiraglia99@gmail.com>
2019-05-29 12:41:45 -0400

    added code to subscript test1

--- project/sub_script1.py
+++ project/sub_script1.py
@@ -8,11 +8,28 @@ print (iqf)
 
 #can't trust line numbers, can trust format and patterns 
 
-s1 =   "1  Dayhoff       34598.308    63  69322.616    69333.353    69618.917"
+s1 = "  1  Dayhoff       34598.308    63  69322.616    69333.353    69618.917"
 s1.strip()
+list1 = s1.split()
+for in in list1:
+	print (i, end= " ") 	#this prints the string without commas, must ammend this list!!!
+	
+	
+
 
 #start using regular expressions
 #notes for molly when she comes back to this: regular expression operators
+
+#pseudocode:
+#1. remove spaces
+#2. turn into string into list into a string
+
+
+
+
+
+
+
 
 
 
