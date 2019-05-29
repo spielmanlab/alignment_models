26f380d757a8d1e9ee7685acd08e1f17186a06cb
Molly Miraglia <mlmiraglia99@gmail.com>
2019-05-29 17:39:15 -0400

    modified subscript1

--- project/sub_script1.py
+++ project/sub_script1.py
@@ -11,10 +11,16 @@ print (iqf)
 s1 = "  1  Dayhoff       34598.308    63  69322.616    69333.353    69618.917"
 s1.strip()
 list1 = s1.split()
-for in in list1:
-	print (i, end= " ") 	#this prints the string without commas, must ammend this list!!!
+print (list1)
+
+# doesn't work: [
+#for item in list1:
+	# print (i, end= " ") 	
+	#]
+
+#this prints the string without commas, must ammend this list!!!
 	
-	
+print (",".join(list1))
 
 
 #start using regular expressions
