ec80af52f67e682331916277eee171e70bbe2996
Molly Miraglia <mlmiraglia99@gmail.com>
2019-06-04 19:53:20 -0400

    added pseudocode to what i think i need to do for fullparse

--- project/full_script1.py
+++ project/full_script1.py
@@ -8,6 +8,23 @@ outfile = open ("fullparsed.txt", "w")
 iqline = iqf.read()
 
 
+while 
+
+
+for line in iqline:
+	items = line.split()
+	if line == "ModelFiner"
+
+#line needs to be split, so i can index it
+#if line starts with modelfinder, then begin parsing
+#if line starts with akaike, then end parsing
+#need to do a while statement, to make each line a list, then a for statement 
+# with an if statement to find starting
+
+#make a function from sub_script1.py while statement(?)
+
+
+
 for line in iqline: 
 	items = iqline.strip()
 	dataline =  (",".join(items))
