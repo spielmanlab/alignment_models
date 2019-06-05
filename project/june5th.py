5a2cdb1af2b3a3b3dd19f58d5e2894fe456bd8e9
Molly Miraglia <mlmiraglia99@gmail.com>
2019-06-05 15:09:13 -0400

    notes from june 5th's meeting

---
+++ project/june5th.py
@@ -0,0 +1,20 @@
+
+
+#example: 
+
+
+#aln1.fasta = infile
+#aln1_models = outfile  
+
+# infile.split(".", [0]) = outfile 
+
+
+#outfile = infile.replace (".fasta")s
+
+
+#list of aln files:
+#for file in alnfiles:
+	#run it through iqtree
+	#parse to make it a csv
+	
+	
