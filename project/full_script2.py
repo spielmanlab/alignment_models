003c1c9200855efeff8b7ee7cc40a40229507a1d
Molly Miraglia <mlmiraglia99@gmail.com>
2019-06-10 21:01:03 -0400

    adding progress to lookback on

--- project/full_script2.py
+++ project/full_script2.py
@@ -1,8 +1,14 @@
 infile = open ("iqtree_full.txt", "r")
 outfile = open ("fullparsed2.txt", "w")
-content = infile.read()
-iqline = infile.readline()
-iqlines = infile.readlines()
+#content = infile.read()
+#iqline = infile.readline()
+#iqlines = infile.readlines()
+#memory don't read 3 times
+parse1 = False 
+parse2 = True 
+
+
+
 
 i = 0
 for line in infile:
@@ -11,6 +17,16 @@ for line in infile:
 	print (item)
 	if i >= 9:
 		break
+		
+		
+for line in infile:
+	print (line)
+	if line.startswith(" No. Model")
+	
+
+#if it is, then keep using that, then when it is not, then stop:
+#parse - keep it true until it isn't, then you can break when it's false (after akaike)
+#loop over the file, then if, and then another for statment, like three lines of code?
 		
 		
 	
