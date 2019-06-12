b841d800300040bc14d9b50b4cb9200e1e5341c2
Molly Miraglia <mlmiraglia99@gmail.com>
2019-06-12 08:33:32 -0400

    full_script 4 prints what i want but now i need to figure out where to stop

--- project/full_script4.py
+++ project/full_script4.py
@@ -1,1 +1,23 @@
-aaaaaq
+infile = open ("iqtree_full.txt", "r")
+outfile = open ("fullparsed2.txt", "w")
+iqline = infile.readline()
+sta = " No. "
+stop = " Akaike"
+start_parsing = False
+
+
+
+for line in infile:
+	if line.startswith(sta):
+		start_parsing = True
+		if start_parsing:
+			for line in infile:
+					items = line.rsplit()
+					dataline = (",".join(items))
+					outfile.write(dataline + "\n")
+					print (dataline)
+		else:
+			break
+		
+
+			 
