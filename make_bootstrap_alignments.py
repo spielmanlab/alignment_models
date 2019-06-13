c378d51857e25d6b179b3d27ab5239da96dfb818
Stephanie J. Spielman <spielman@rowan.edu>
2019-06-13 12:30:44 -0400

    a directory of gt alignments

--- make_bootstrap_alignments.py
+++ make_bootstrap_alignments.py
@@ -24,6 +24,7 @@ unaligned   = sys.argv[1]        # infile
 alphabet    = sys.argv[2]        # This should be either "protein" or "dna"
 n           =  int(sys.argv[3])  # bootstraps
 numproc     =  int(sys.argv[4])  # threads
+os.environ["OMP_NUM_THREADS"] = str(numproc)
 
 prealn_file = 'prealn.fasta' # Will contain the raw (unaligned) sequences in fasta format
 refaln_file = 'refaln.fasta' # Will contain the reference (unmasked!) alignment
@@ -43,7 +44,7 @@ os.system("cp ../" + unaligned + " " + prealn_file)
 
 # Aligner
 amod = MafftAligner("mafft", " --auto --quiet ")
-tmod = builderFastTree("FastTree", " -fastest -nosupport -quiet ") # -nosupport **MUST** be there
+tmod = builderFastTree("FastTreeMP", " -fastest -nosupport -quiet ") # -nosupport **MUST** be there
 bmod = BootstrapperLight(bootstraps = n, prealn_file = prealn_file, refaln_file = refaln_file, BootDir = BootDir, 
                            threads = numproc, aligner=amod, tree_builder = tmod, srcdir = source)
                            
