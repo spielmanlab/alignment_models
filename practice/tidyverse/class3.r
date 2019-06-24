8b658752a8293541ab3d3108c02a3678db753308
Molly Miraglia <mlmiraglia99@gmail.com>
2019-06-23 21:08:51 -0400

    adding r files

--- practice/tidyverse/class3.r
+++ practice/tidyverse/class3.r
@@ -1,4 +1,20 @@
-install.packages("ggplot2")
-
-
+library(ggplot2)
+#1. Plotting the iris data set
 iris
+attach(iris)
+dataset <- iris
+theme_set(theme_bw(base_size=12))
+#problem1 
+#a
+ggplot (dataset, aes(x=Sepal.Length, y=Petal.Length, color=Species)) + geom_point()
+#b
+ggplot(dataset, aes(x=Sepal.Length, y=Petal.Length, color=Species)) + geom_point() + facet_wrap(~Species)
+#c
+ggplot(dataset, aes(x=Species, y=Sepal.Length)) + geom_boxplot() 
+
+#2. Plotting tree-growth data
+
+sitka <- read.csv("http://wilkelab.org/classes/SDS348/data_sets/sitka.csv")
+head(sitka)
+
+ggplot(sitka, aes(x=Time, y=size, group=tree)) + geom_line() + facet_wrap(~treat)
