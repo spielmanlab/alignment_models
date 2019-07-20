a333148f9de0d96c4daadf205efa304f27b12603
Molly Miraglia <mlmiraglia99@gmail.com>
2019-07-20 07:41:19 -0400

    figure files

---
+++ project/model_csv.R
@@ -0,0 +1,100 @@
+library(tidyverse)
+
+## This function converts a directory of CSV files containing model selection information into a single tibble for all replicates
+## NOTE: Assumes that the following headers are in each CSV: No.,Model,-LnL,df,AIC,AICc,BIC
+## NOTE: Assumes each file is named <name>_alnversion_<number>_models.csv
+process_all_csv <- function(path_to_csv)
+{
+  # List of all csv files in the given directory
+  files <- dir(path = path_to_csv, pattern = "*.csv")
+  tibble(filename = files) %>%
+    mutate(file_contents = map(filename,
+                               ~ read_csv(file.path(path_to_csv, .)))) %>%
+    unnest() %>%
+    separate(filename, c("name", "dummy", "number", "dummy2"), sep="_") %>%
+    dplyr::select(-dummy, -dummy2, -No., -`-LnL`, -df) %>%
+    gather(ic_type, ic_value, AIC, AICc, BIC) %>%
+    group_by(name, number, ic_type) %>%
+    mutate(ic_rank = rank(ic_value)) %>%
+    filter(ic_rank == 1) %>%
+    ungroup() %>%
+    dplyr::select(name, number, Model, ic_type) -> ranked_models
+  ranked_models
+  
+}
+
+aa_path <- "selectome_aa_output/"
+nt_path <- "selectome_nt_output/"
+aa_ranked_models <- process_all_csv(aa_path)
+nt_ranked_models <- process_all_csv(nt_path)
+
+unique(aa_ranked_models$Model)
+
+unique(aa_ranked_models$name) 
+
+
+aa1_unique = "ENSGT00520000055637.Euteleostomi.001.aa.fas"
+
+### one model
+aa_ranked_models %>%
+  filter(name == aa1_unique) %>%
+  ggplot(aes(x = Model, fill = ic_type)) + geom_bar(position = position_dodge()) + theme_classic() -> one_model_plot
+
+ggsave("one_model.pdf", one_model_plot)
+
+two_model = "ENSGT00390000018612.Euteleostomi.001.aa.fas"
+
+### two model
+aa_ranked_models %>%
+  filter(name == two_model) %>%
+  ggplot(aes(x = Model, fill = ic_type)) + geom_bar(position = position_dodge()) + theme_classic() 
+
+### three model
+aa3_unique = "ENSGT00390000013494.Euteleostomi.001.aa.fas"
+aa_ranked_models %>%
+  filter(name == aa3_unique) %>%
+  ggplot(aes(x = Model, fill = ic_type)) + geom_bar(position = position_dodge()) + theme_classic() 
+
+
+test = "ENSGT00390000005703.Euteleostomi.001.aa.fas"
+
+aa_ranked_models %>%
+  filter(name == test) %>%
+  ggplot(aes(x = Model, fill = ic_type)) + geom_bar(position = position_dodge()) + theme_classic() 
+
+for (name in unique(aa_ranked_models$name)) {
+  readline(name)
+}
+
+
+unique(nt_ranked_models$name)
+
+one_model = "ENSGT00390000000455.Euteleostomi.001.nt.fas"
+
+### one model
+nt_ranked_models %>%
+  filter(name == one_model) %>%
+  ggplot(aes(x = Model, fill = ic_type)) + geom_bar(position = position_dodge()) + theme_classic() + 
+  scale_y_continuous(name = "Number of Alignments") -> one_nt_model
+ggsave("one_nt_model.pdf", one_nt_model)
+
+
+three_model = "ENSGT00410000025793.Euteleostomi.001.nt.fas"
+### three model
+nt_ranked_models %>%
+  filter(name == three_model) %>%
+  ggplot(aes(x = Model, fill = ic_type)) + geom_bar(position = position_dodge()) + theme_classic() +
+  scale_y_continuous(name = "Number of Alignments") -> three_nt_model
+ggsave("three_nt_model.pdf", three_nt_model)
+
+
+six_nt_model = "ENSGT00390000008002.Euteleostomi.001.nt.fas"
+
+### 6 models
+nt_ranked_models %>%
+  filter(name == six_nt_model) %>%
+  ggplot(aes(x = Model, fill = ic_type)) + geom_bar(position = position_dodge()) + theme_classic() +
+  scale_y_continuous(name = "Number of Alignments") -> nt_six_model
+ggsave("six_nt_model.pdf", nt_six_model)
+
+
