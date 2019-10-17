0291835371551c4d7b5f9f116674b9761cd59967
Molly Miraglia <miragl82@students.rowan.edu>
2019-10-16 20:04:30 -0400

    file cleanup and found lost file

--- project/one_csv/full_dendropy.py
+++ project/one_csv/full_dendropy.py
@@ -16,10 +16,10 @@ aa_path_to_alignments = "../selectome_aa_output/"
 
 
 path_to_aa_output_trees = aa_path_to_alignments + "alnversion1_trees/"
-os.system("mkdir -p " + path_to_aa_output_trees)
+#os.system("mkdir -p " + path_to_aa_output_trees)
 
-path_to_nt_output_trees = nt_path_to_alignments + "alnversion1_trees/"
-os.system("mkdir -p" + path_to_nt_output_trees)
+path_to_nt_output_trees = nt_path_to_alignments + "alnvers1_trees/"
+os.system("mkdir -p " + path_to_nt_output_trees)
 
 aa_all = os.listdir(aa_path_to_alignments)
 nt_all = os.listdir(nt_path_to_alignments)
@@ -27,9 +27,6 @@ nt_all = os.listdir(nt_path_to_alignments)
 file_ending = ".fas_alnversions"
 file_a1ending = "alnversion_1.fasta"
 
-
-
- 
 #aa files for fast tree
 for file in aa_all:
     name = str(file)
@@ -37,7 +34,7 @@ for file in aa_all:
     #### If variable `file` is a DIRECTORY, head into here:
     if x: 
         aa_files = os.listdir(aa_path_to_alignments + file)
-        #print(aa_files)
+       #print(aa_files)
         for a1 in aa_files:
             aa_file_ending = a1.endswith(file_a1ending)
             if aa_file_ending:
@@ -45,11 +42,10 @@ for file in aa_all:
                 #print(aa_output_tree_file)
                 
                 ### run the tree
-                os.system("FastTree -nosupport -quiet " + aa_path_to_alignments + name + str(a1) + " > " + aa_output_tree_file)    
-        #print(aa_output_tree_file
-    #aa_tree = dendropy.Tree.get(
-     #   path=aa_output_tree_file,
-      #  schema="newick")  
+                os.system("FastTree -nosupport -quiet " + aa_path_to_alignments + name + "/" + str(a1) + " > " + aa_output_tree_file)   
+                #aa_tree = dendropy.Tree.get(
+                    #path=aa_output_tree_file,
+                    #schema="newick")  
     #print(aa_tree.length())
     #aa_pdc = aa_tree.phylogenetic_distance_matrix()
 #print(pdc.mean_pairwise_distance())
@@ -66,13 +62,10 @@ for file in nt_all:
             if nt_file_ending:
                 nt_output_tree_file = path_to_nt_output_trees + n1 + ".tree"
                 #print(nt_output_tree_file)
-                os.system("FastTree -nt -gtr -nosupport -quiet" + nt_path_to_alignments + name + str(n1) + " > " + nt_output_tree_file)
-   
-    
-    
-    #nt_tree = dendropy.Tree.get(
-        #path = nt_output_tree_file,
-        #schema ="newick")
+                #os.system("FastTree -nt -gtr -nosupport -quiet " + nt_path_to_alignments + name + "/" + str(n1) + " > " + nt_output_tree_file)
+                nt_tree = dendropy.Tree.get(
+                    path = nt_output_tree_file,
+                    schema ="newick")
     #print(nt_tree.length())
     #nt_pdc = nt_tree.phylogenetic_distance_matrix()
 #print(pdc.mean_pairwise_distance())
