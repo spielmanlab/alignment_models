2626dd277333dd1f499700f8c86a9862bfbc725a
Molly Miraglia <miragl82@students.rowan.edu>
2020-02-05 12:32:11 -0500

    edited my rmd and my r script for csv tidy:

--- project/scripts/all_csv_bestmodels.r
+++ project/scripts/all_csv_bestmodels.r
@@ -5,11 +5,7 @@ library(plyr)
 csv_directory <- "../output_selectome"
 #dir() - lists all the files in a directory 
 csv_files <- dir(path=csv_directory,pattern="*.csv")
-file_tibble <- tibble(filename=csv_files) 
-ranked <- file_tibble %>%
-  mutate(file_contents=map(filename,
-                            ~ read_csv(file.path(csv_directory, .)))) %>%
-  unnest() 
+ 
 ranked %>%
   separate(name,c("name","species","no"),sep="[.]") %>%
   select(-filename,-no)
