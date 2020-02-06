5da31ab955dd9a72bd1ede3692e9658794751295
Molly Miraglia <miragl82@students.rowan.edu>
2020-02-06 14:34:13 -0500

    trying to improve rmd

--- project/scripts/all_csv_bestmodels.r
+++ project/scripts/all_csv_bestmodels.r
@@ -5,9 +5,19 @@ library(plyr)
 csv_directory <- "../output_selectome"
 #dir() - lists all the files in a directory 
 csv_files <- dir(path=csv_directory,pattern="*.csv")
- 
-ranked %>%
+afile_tibble <- tibble(filename=csv_files) 
+csvdf <- afile_tibble %>%
+  mutate(file_contents=map(filename,
+                           ~ read_csv(file.path(csv_directory, .)))) %>%
+  unnest() 
+
+csvdf %>%
   separate(name,c("name","species","no"),sep="[.]") %>%
-  select(-filename,-no)
+  select(-filename,-no) %>%
+  tally()
   
+  
+
+#then then 
+
 #ranked
