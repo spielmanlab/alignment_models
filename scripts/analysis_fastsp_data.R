e1a17a3140b15788f56db9103cab314356d4ea1a
Molly Miraglia <mlmiraglia99@gmail.com>
2020-06-25 12:25:58 -0400

    analysis script update

--- scripts/analysis_fastsp_data.R
+++ scripts/analysis_fastsp_data.R
@@ -1,5 +1,17 @@
 library(tidyverse)
+#will start with just one .csv file and then continue with all
+path_drossaa <- "results/Drosophila_AA_alignment_scores.csv"
+dros_aa <-read_csv(path_drossaa)
 
-csv_dir <-"results/Drosophila_AA_alignment_scores.csv"
-
-read_csv(csv_dir)
+i<-0
+while(i<=50) {
+  dros_aa %>%
+    group_by(dataset,ref_num,est_num,sp,tc) 
+    
+}
+  
+stat_csv <- dros_aa %>%
+  select(-species,-datatype) %>%
+  group_by(dataset,ref_num,est_num,sp,tc) %>%
+  tally() %>%
+  select()
