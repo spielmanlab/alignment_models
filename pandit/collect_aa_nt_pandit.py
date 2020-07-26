0e2b58606f6111fbcf11089b363af0a7a089f91b
Stephanie J. Spielman <spielman@rowan.edu>
2020-07-26 18:23:33 -0400

    correct PANDIT data

--- pandit/collect_aa_nt_pandit.py
+++ pandit/collect_aa_nt_pandit.py
@@ -1,8 +1,7 @@
 """
 From SJS personal zip of the PANDIT database, get all the compatible data.
-MUST sed OUT THE GAPS AFTER RUNNING THIS!!! (below, I have gsed as my GNU instead of BSD default on Mac)
-gsed -i "s/-//g" PANDIT_AA/*
-gsed -i "s/-//g" PANDIT_NT/*
+Name output files to be compatible with Selectome naming (for scripting ease): <NAME>.nt.fas and <NAME>.aa.fas
+====> 254 PANDIT alignments
 """
 
 from Bio import SeqIO
@@ -10,10 +9,8 @@ import random
 import os
 import sys
 
-min_seq = 20
-max_seq = 1000
-min_len = 100  ## AA length
-max_len = 5000 ## AA length
+min_seq = 25
+min_len = 100
 outdir_aa = "PANDIT_AA/"
 outdir_nt = "PANDIT_NT/"
 
@@ -21,40 +18,73 @@ path_to_pandit = sys.argv[1]
 
 aa_fastas = [x for x in os.listdir(path_to_pandit) if x.endswith("_aa.fasta")]
 
-contenders_sites = {}
-contenders_seqs  = {}
-contenders = []
 
+ok = 0
 for pandit_aa in aa_fastas:
 
     pandit_name = pandit_aa.split("_")[0]
     pandit_nuc = pandit_name + "_nuc.fasta"
     
+    outfile_aa = outdir_aa + pandit_name + ".aa.fas"
+    outfile_nuc = outdir_nt + pandit_name + ".nt.fas"
+    
     with open(path_to_pandit + pandit_aa, "r") as f:
         aa_recs = list(SeqIO.parse(f, "fasta"))
-    nseq = len(aa_recs)
-    nsites = len(aa_recs[0])
     aa_names = [str(x.id) for x in aa_recs]
     aa_names.sort()
-    
-    seq_ok = nseq >= min_seq and nseq <= max_seq
-    len_ok = nsites >= min_len and nsites <= max_len
-    
-    
-    if seq_ok and len_ok:
+        
+    gapless_aa = {}
+    gapless_lengths = []
+    for rec in aa_recs:
+        aa_seq = str(rec.seq).replace("-", "")
+        gapless_aa[str(rec.id)] = aa_seq
+        gapless_lengths.append(len(aa_seq))
+    conditions_ok = min(gapless_lengths) >= min_len and len(aa_recs) >= min_seq
+  
+    if conditions_ok:
         if os.path.exists(path_to_pandit + pandit_nuc):
             with open(path_to_pandit + pandit_nuc, "r") as f:
                 nuc_recs = list(SeqIO.parse(f, "fasta"))
                 nuc_names = [str(x.id) for x in nuc_recs]
                 nuc_names.sort()
                 
-                ## FINAL MATCH CONDITION
+                ## same nuc and aa records
                 if (aa_names == nuc_names):
-                    os.system("cp " + path_to_pandit+"/" + pandit_nuc + " " + outdir_nt)
-                    os.system("mv " + outdir_nt + pandit_nuc + " " + outdir_nt + pandit_nuc.replace("nuc", "nt"))
-                    os.system("cp " + path_to_pandit+"/" + pandit_aa + " " + outdir_aa)
+                    gapless_nuc = {}
+                    for rec in nuc_recs:
+                        nuc_seq = str(rec.seq).replace("-", "")
+                        gapless_nuc[str(rec.id)] = nuc_seq
+                        
+                        corresponding_aa_len = len( gapless_aa[str(rec.id)] )
+                        if corresponding_aa_len*3 != len(nuc_seq):
+                            print(pandit_name, "Incompatible nuc/aa")
+                            continue
                     
+                    ##### Here, fully checked nt and aa. Save to files
+                    with open(outfile_aa, "w") as f:
+                        for record in gapless_aa:
+                            f.write(">" + str(record) + "\n" + str(gapless_aa[record]) + "\n")
+                    with open(outfile_nuc, "w") as f:
+                        for record in gapless_nuc:
+                            f.write(">" + str(record) + "\n" + str(gapless_nuc[record]) + "\n")
                     
+                    ok += 1
+                    
+
+                else:
+                    print(pandit_name, "Names don't match")
+                    continue
+        else:
+            print(pandit_name, "No nuc file")
+            continue
+    else:
+        print(pandit_name, "Conditions not met")
+        continue
+    
+
+print("\n\n")
+print("------------TOTAL------------")
+print(ok)
                     
                     
                     
