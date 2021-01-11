fd79a660a6b113779ea591d517ef857f4ef21a13
Stephanie J. Spielman <spielman@rowan.edu>
2021-01-11 17:43:12 -0500

    code for linear and logistic regressions and their associated figures

---
+++ results/load_prepare_data.R
@@ -0,0 +1,43 @@
+
+models <- read_csv("all_selected_models.csv") %>% process_raw_models() # note: this has a warning and it's fine
+scores <- read_csv("all_alignment_scores.csv")
+hamming <- read_csv("pairwise_hamming_distances.csv") %>% rename(id = dataname)
+data_info <- read_csv("nsites_nseqs.csv") %>% rename(id = name)
+
+## Create summary data frames ---------------------------------------------
+# How many datasets are there? 1000 for each selectome and 236 PANDIT - yup!
+models %>% 
+  filter(ic_type == "AIC", num == 1) %>% 
+  count(dataset, datatype, name = "total") -> number_of_datasets
+
+# How many models per dataset?  
+models %>%
+  group_by(id, datatype, dataset, ic_type) %>%
+  count(best_model) %>%
+  ungroup() %>%
+  count(id, datatype, dataset, ic_type, name = "n_models") -> how_many_models
+
+# How many matrices per dataset?
+models %>%
+  group_by(id, datatype, dataset, ic_type) %>%
+  count(best_matrix) %>%
+  ungroup() %>%
+  count(id, datatype, dataset, ic_type, name = "n_matrices") -> how_many_matrices
+
+# Percent of dataset variants with top model?
+models %>%
+  group_by(id, datatype, dataset, ic_type) %>%
+  count(best_model) %>%
+  ungroup() %>%
+  group_by(id, datatype, dataset, ic_type) %>%
+  summarize(top_model_percent = max(n)/total_reps) %>%
+  ungroup()-> percent_top_models
+
+# Percent of dataset variants with top matrix?
+models %>%
+  group_by(id, datatype, dataset, ic_type) %>%
+  count(best_matrix) %>%
+  ungroup() %>%
+  group_by(id, datatype, dataset, ic_type) %>%
+  summarize(top_matrix_percent = max(n)/total_reps) %>%
+  ungroup()-> percent_top_matrices
