30dbe1b4ceb6c3d30a43e49c4be4ac4b2d7ff420
Stephanie J. Spielman <spielman@rowan.edu>
2021-02-03 12:18:21 -0500

    mean,median,sd MSA guidance scores

--- scripts/calculate_guidance_scores.py
+++ scripts/calculate_guidance_scores.py
@@ -3,7 +3,8 @@ Modified code from:  https://github.com/sjspielman/alignment_filtering/blob/mast
 """
 from Bio import AlignIO
 import numpy as np
-
+import sys
+import os
 
 def count_gaps(parsed, alnlen): #parsed = a parsed alignment.
     ## Count the number of gaps in a column and returns a list. Does not give any information as to which positions are gaps.         
@@ -44,35 +45,59 @@ def process_scores(n, numseq, alnlen, parsed_msa, all_scores):
     
     return (final_scores)
     
-        
-
 n_boot = 49
 guidance_scorer = "./guidance_score"
-reference_msa_file = sys.argv[1]
-with open(reference_msa, "r") as f:
-    reference_msa_records = AlignIO.read(f, "fasta")
-numseq = len(reference_msa_records)
-alnlen = len(reference_msa_records[0].seq)
-all_scores=np.zeros((numseq, alnlen)) 
 
-for i in range(n_boot):
-    test_msa_file = ""
-    outfile = reference_msa_file.replace(".fasta", "_guidance_scores.txt")
+final_scores_mean_med_sd = {}
+
+
+path_to_everything = sys.argv[1]
+subpaths = [x for x in os.listdir(path_to_everything) if x.startswith("perturbed")]
+for path2 in subpaths:
+    j = 0
+    full_path2 = os.path.join(path_to_everything, path2) + "/"
+    subsubpaths =  [x for x in os.listdir(full_path2) if os.path.isdir(full_path2+x)]
+    for dataset_path in subsubpaths:
+        j += 1
+        print(path2, j)
+        # BEGIN #
+        msa_path = os.path.join(path_to_everything, path2, dataset_path) + "/"
+        overall_name = dataset_path.rstrip("/")
+        reference_msa_file = os.path.join(msa_path, overall_name + "_50.fasta")
+
+        with open(reference_msa_file, "r") as f:
+            reference_msa_records = AlignIO.read(f, "fasta")
+        numseq = len(reference_msa_records)
+        alnlen = len(reference_msa_records[0].seq)
+        all_scores=np.zeros((numseq, alnlen)) 
+
+        for i in range(1, n_boot+1):
+            test_msa_file = reference_msa_file.replace("_50.fasta", "_" + str(i) + ".fasta")
+            outfile = "temp.txt"
     
-    cmd = " ".join([guidance_scorer, reference_msa_file, test_msa_file, outfile])
-    cmdcode = os.system(cmd)
-    assert(cmdcode == 0), "error: guidance scoring"
-    scores = np.loadtxt(outfile)
-    all_scores = all_scores + scores
+            cmd = " ".join([guidance_scorer, reference_msa_file, test_msa_file, outfile])
+            cmdcode = os.system(cmd)
+            assert(cmdcode == 0), "error: guidance scoring"
+            scores = np.loadtxt(outfile)
+            all_scores = all_scores + scores
     
-final_scores = process_scores(n_boot, numseq, alnlen, reference_msa_records, all_scores)
-
-
-
-
-
-
-
-
-
-
+        final_scores = process_scores(n_boot, numseq, alnlen, reference_msa_records, all_scores)
+        flat_scores = np.ndarray.flatten(final_scores)
+        final_scores_mean_med_sd[overall_name] = [str(np.mean(flat_scores)), str(np.median(flat_scores)), str(np.std(flat_scores))]
+        #print(final_mean_scores)
+        #print(final_median_scores)
+        #print(final_sd_scores)
+        #break
+        
+with open("../results/final_guidance_scores.csv", "w") as f:
+    f.write("fullid,mean_guidance_score,median_guidance_score,sd_guidance_score\n")
+    for key in final_scores_mean_med_sd:
+        thiskey = final_scores_mean_med_sd[key]
+        f.write(",".join([key, thiskey[0], thiskey[1], thiskey[2]]) + "\n")
+    
+    
+    
+    
+    
+    
+    
